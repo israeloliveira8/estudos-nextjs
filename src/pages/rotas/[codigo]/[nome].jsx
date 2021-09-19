dd25a29a89d3f6aa7c562e46f06a04cea0ed2990
Israel Oliveira <israel.oliveira.933@gmail.com>
2021-09-19 12:24:16 -0300

    feat: implementacoes

---
+++ src/pages/rotas/[codigo]/[nome].jsx
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+export default function CodigoENome() {
+    const router = useRouter()
+    const codigo = router.query.codigo
+    const nome = router.query.nome
+    
+    return (
+        <div>
+            <h1>Rotas / {codigo} / {nome}</h1>
+            <Link href="/rotas" passHref>
+                <button>Voltar</button>
+            </Link>
+        </div>
+    )
+}
+
+// rotas dinamicas carregam consigo um par
+// de colchetes... Exemplo: [arquivo]
